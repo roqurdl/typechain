dd6d59ca0c12af37b66ab3bc64c4e1aee6e7cf51
roqurdl <uyhnri@gmail.com>
2022-08-24 17:17:09 +0900

    beforeTyped

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-import { init, exit } from "package";
+import crypto from "crypto";
 
-init({
-  debug: true,
-  url: "false",
-});
+interface BlockShape {
+  hash: string;
+  prehash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prehash: String,
+    public height: Number,
+    public data: String
+  ) {
+    this.hash = Block.calculateHash(prehash, height, data);
+  }
+  static calculateHash(prehash: String, height: Number, data: String) {
+    const toHash = `${prehash}${height}${data}`;
+  }
+}
